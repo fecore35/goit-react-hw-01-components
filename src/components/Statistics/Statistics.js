61ea0df45a41063df1151c4eb8de8d6c6aa2ff74
Evgenii Bryzgun <e.bryzgun@gmail.com>
2021-10-02 00:30:17 +0300

    Добавляю стили Statistics

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,16 +1,17 @@
 import PropTypes from "prop-types";
+import s from "./Statistics.module.css";
 
 function Statistics({ title, stats }) {
   return (
-    <section className="statistics">
-      {title && <h2 className="title">{title}</h2>}
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title}</h2>}
 
-      <ul className="stat-list">
+      <ul className={s.list}>
         {stats.map(({ id, label, percentage }) => {
           return (
-            <li key={id} className="item">
-              <span className="label">{label}</span>
-              <span className="percentage">{percentage}%</span>
+            <li key={id} className={s.item}>
+              <span className={s.label}>{label}</span>
+              <span className={s.percentage}>{percentage}%</span>
             </li>
           );
         })}
