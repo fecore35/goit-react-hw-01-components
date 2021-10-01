356e39be1dcef0e24e62ad91412f5259c3d762b0
Evgenii Bryzgun <e.bryzgun@gmail.com>
2021-10-01 17:21:57 +0300

    Добавляю компонент Statistics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Button from "components/Button/Button";
+import user from "./db/user.json";
+import statisticalData from "./db/statistical-data.json";
+import Profile from "components/Profile/Profile";
+import Statistics from "components/Statistics/Statistics";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button />
+        <Profile
+          name={user.name}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+        <Statistics title="Upload stats" stats={statisticalData} />;
+        <Statistics stats={statisticalData} />;
       </header>
     </div>
   );
