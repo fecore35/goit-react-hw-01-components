ceebdff475ca757a7f224b6dc2e7b182d3883f0e
Evgenii Bryzgun <e.bryzgun@gmail.com>
2021-10-01 17:46:33 +0300

    fix: App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import user from "./db/user.json";
-import statisticalData from "./db/statistical-data.json";
+import user from "../../db/user.json";
+import statisticalData from "../../db/statistical-data.json";
 import Profile from "components/Profile/Profile";
 import Statistics from "components/Statistics/Statistics";
 
@@ -14,8 +14,8 @@ function App() {
           avatar={user.avatar}
           stats={user.stats}
         />
-        <Statistics title="Upload stats" stats={statisticalData} />;
-        <Statistics stats={statisticalData} />;
+        <Statistics title="Upload stats" stats={statisticalData} />
+        <Statistics stats={statisticalData} />
       </header>
     </div>
   );
