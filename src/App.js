3bf1e3b369663e318fe71d78112603f16689a74c
Evgenii Bryzgun <e.bryzgun@gmail.com>
2021-10-01 21:36:45 +0300

    Добавляю компонент TransactionHistory

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import user from "./db/user.json";
 import statisticalData from "./db/statistical-data.json";
 import friends from "./db/friends.json";
+import transactions from "./db/transactions.json";
 import Profile from "components/Profile/Profile";
 import Statistics from "components/Statistics/Statistics";
 import FriendList from "components/FriendList/FriendList";
+import TransactionHistory from "components/TransactionHistory/TransactionHistory";
 
 function App() {
   return (
@@ -18,8 +20,8 @@ function App() {
         />
         <Statistics title="Upload stats" stats={statisticalData} />
         <Statistics stats={statisticalData} />
-
         <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />;
       </header>
     </div>
   );
